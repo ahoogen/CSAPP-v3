0880967f6f971dc62bbb12f5b27e3be67f25e07c
Austen Hoogen <ahoogen@gmail.com>
2018-03-02 22:02:52 -0800

    Implemented the required functions, refactored to split out common 'show_bytes' logic, and updated show_bits to use byte_pointer type Refactored for formatting and readability

--- 2/2.57.show_bits.c
+++ 2/2.57.show_bits.c
@@ -4,32 +4,76 @@ byte representations of C objects of types short, long and double, respectively.
 */
 #include <stdio.h>
 
+typedef unsigned char *byte_pointer;
+
 /*
-print_bits shows the binary representation of a number from left to right.
+show_bits shows the binary representation of a number from left to right.
 Depending on the architecture, most significant to least significant, or vice versa.
 */
-void print_bits(long p) {
-    int s = sizeof(p) * 8 - 1;
-    printf("Integer %ld has %d bits represented by binary value:\n", p, s + 1);
-    for (;  s >= 0; s--) {
-        if (p & 1 << s) {
-            printf("1");
-        } else {
-            printf("0");
-        }
+void show_bits(byte_pointer p, int b) {
+    if (b < 0 || b > sizeof(p)) {
+        printf("Number of bytes %d is out of bounds of byte_pointer.\n", b);
+        return;
+    }
 
-        if (s % 4 == 0) {
-            printf(" ");
+    int j;
+    for (;  b > 0; b--) {
+        for (j = 7; j >= 0; j--) {
+            if (p[b-1] & 1 << j) {
+                printf("1");
+            } else {
+                printf("0");
+            }
+
+            if (j % 4 == 0) {
+                printf(" ");
+            }
         }
     }
     printf("\n");
+}
 
+// Show b bytes of byte_pointer x
+void show_bytes(byte_pointer x, int b) {
+    printf("0x");
+    for (; b > 0; b--) {
+        printf("%02X", x[b-1]);
+    }
+    printf("\n");
+}
+
+void show_short(short int x) {
+    printf("Short int: %d\nNumber of bytes: %ld\nHexadecimal: ", x, sizeof(x));
+    show_bytes((byte_pointer)&x, sizeof(x));
+    printf("Binary: ");
+    show_bits((byte_pointer)&x, sizeof(x));
+    printf("\n");
+}
+
+void show_long(long x) {
+    printf("Long int: %ld\nNumber of bytes: %ld\nHexadecimal: ", x, sizeof(x));
+    show_bytes((byte_pointer)&x, sizeof(x));
+    printf("Binary: ");
+    show_bits((byte_pointer)&x, sizeof(x));
+    printf("\n");
+}
+
+void show_double(double x) {
+    printf("Double float: %3.30f\nNumber of bytes: %ld\nHexadecimal: ", x, sizeof(x));
+    show_bytes((byte_pointer)&x, sizeof(x));
+    printf("Binary: ");
+    show_bits((byte_pointer)&x, sizeof(x));
+    printf("\n");
 }
 
 int main()
 {
-    long x = 482263343;
-    print_bits(x);
+    short s = 16232;
+    long l = 2111598399;
+    double d = 3.141592653589793238462643383279;
+    show_short(s);
+    show_long(l);
+    show_double(d);
 
     return 0;
 }
