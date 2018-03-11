f5bfd0a8b6da45ece7cbeb120e588dbe474e600d
Austen Hoogen <ahoogen@gmail.com>
2018-03-11 13:34:30 -0700

    Expanded explanation of 3.58: decode2

--- 3/3.58.decode2.c
+++ 3/3.58.decode2.c
@@ -32,6 +32,23 @@ decode2:    S     D
     sarq    $63, %rax       # y >>= 63
     xorq    %rdi, %rax      # y ^= x
     ret                     # return y
+
+The basic operations are described above, which can be reduced to:
+y -= z
+x *= y
+y << 63 >> 63
+return y ^ x
+
+With the movq placing the value of y (after manipulation) into register %rax we
+get a picture that we've declared a variable as a return value, I'll call 'a'.
+
+long a = y - z;
+
+I *could* declare another variable for the manipulation of x, but since no other
+registers are referenced I'm going to assume that the total operation proceeds
+with declarations on hand, so that gives us:
+
+return (a * x) ^ (y << 63 >> 63)
 */
 
 long decode2(long x, long y, long z) {
